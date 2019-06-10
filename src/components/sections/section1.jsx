a3bfcbbd74dd12132cb0549be4312521e4938b27
BadassBison <shawn.scott.xd@gmail.com>
2019-06-10 08:51:11 -0700

    Add skills images

---
+++ src/components/sections/section1.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Section = () => {
+    return (
+        <section className="section section-light">
+            <h2>A little about me...</h2>
+            <p>
+                Big test
+            </p>
+        </section>
+    )
+}
+
+export default Section;
