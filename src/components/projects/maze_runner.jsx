a72de7db67dfe39bc2d95837ae9af12c0b543ee9
BadassBison <shawn.scott.xd@gmail.com>
2019-06-21 10:30:27 -0700

    Update title for maze runner project

--- src/components/projects/maze_runner.jsx
+++ src/components/projects/maze_runner.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Project = () => {
     return (
         <div className="project">
-            <h2>Uncappd - <a href="https://github.com/BadassBison/Maze_Runner" target="_blank">Github</a></h2>
+            <h2>Maze Runner - <a href="https://github.com/BadassBison/Maze_Runner" target="_blank">Github</a></h2>
             <a href="https://github.com/BadassBison/shawnscott.us.git" target="_blank">
                 <img src="assets/maze_runner.png" name="maze_runner" alt="Maze Runner"/>
             </a>
